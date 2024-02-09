7e3c9d30cedb7c303ff960addcbd7721957960f9
Avinash Kumar <avinash.kumar2@gyansys.com>
2024-02-09 17:24:01 +0530

    JavaScript Assignment

---
+++ workarea/avinash-kumar/JavaScript-training/Calc.js
@@ -0,0 +1,28 @@
+
+
+function solve(val){
+    var v=document.getElementById('res');
+    v.value+=val;
+}
+
+function result(){
+    var num1 = document.getElementById('res').value;
+   var num2 = eval(num1);
+   document.getElementById('res').value = num2;
+   document.getElementById('result-container').innerHTML=`
+   <h2>Result: ${num2}</h2>
+   `
+}
+
+
+function Clear(){
+    var ev = document.getElementById('res');
+         ev.value = '';
+}
+
+function back(){
+    var ev = document.getElementById('res');
+         ev.value = ev.value.slice(0,-1);
+}
+
+alert("Calculate Your something")

0e6a891f83c6f16c483f6a97a0ee43d83eb8a947
Avinash Kumar <avinash.kumar2@gyansys.com>
2024-02-12 21:51:00 +0530

    added login page ,signup,and more responsive and added some javascript

--- workarea/avinash-kumar/html-css-training/javascript.js
+++ workarea/avinash-kumar/html-css-training/javascript.js
@@ -1,1 +1,16 @@
-alert("welcome ")
+
+const e1=document.getElementById('btn1');
+function func1(){
+    e1.nextElementSibling.classList.toggle('show');
+    
+}
+const e3=document.getElementsByClassName('nav');
+const e2=document.getElementById('icon');
+
+
+function funct2(){
+        console.log(e2.nextElementSibling);
+        e2.nextElementSibling.classList.toggle('show');
+}
+
+

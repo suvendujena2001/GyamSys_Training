c879f190035d46ef844749b2bed92880be45030c
Aditya Raj thakur <aditya.thakur@gyansys.com>
2024-02-15 09:29:27 +0530

    14.02

--- workarea/aditya-raj-thakur/Javascript/script.js
+++ workarea/aditya-raj-thakur/Javascript/script.js
@@ -1,4 +1,36 @@
 
+// call by value 
+let a=1;
+let b=a;
+b=2;
+console.log("a="+a);
+console.log("b="+b);
+
+
+//call by reference
+let arr=["x","y","z"];
+let arr1=arr;
+arr1.push("x");
+console.log(arr);
+console.log(arr1)
+ 
+//map and filter
+let arr2=[1,2,3,4,5];
+let arr3=arr2.filter(x=>x>2);
+console.log(arr3);
+let arr4=arr2.map(x=>x-1);
+console.log(arr4);
+
+//object creation
+let obj={
+    name:'adi',
+    degree:'btech',
+    course:'cse'};
+console.log(obj);
+
+
+// // ---------------------------------------------------------------------------------------------------------------
+
 /// 14-02-2024 --->
 
 let empNames=['adi','abhi','avi','prince','oggy','abhi'];

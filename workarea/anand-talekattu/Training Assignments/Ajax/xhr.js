8634ce4ee5d3e3d86c007181221acc327186fde3
anand2165 <anand.mallikarjun@gyansys.com>
2024-02-19 16:02:50 +0530

    updated ajax file

--- workarea/anand-talekattu/Training Assignments/Ajax/xhr.js
+++ workarea/anand-talekattu/Training Assignments/Ajax/xhr.js
@@ -2,8 +2,11 @@ let xhr=new XMLHttpRequest();
 let url="https://jsonplaceholder.typicode.com/users";
 xhr.onreadystatechange=function(){
   if(this.readyState==4 && this.status==200){
-   
-console.log(xhr.response);
+   var data=xhr.response;
+   let con=document.getElementById("a1");
+   for(var i=0;i<data.length;i++){
+    con.innerHTML += "<div>"+data[i].name+"</div>";
+   }
   }
 }
 xhr.open("GET",url);

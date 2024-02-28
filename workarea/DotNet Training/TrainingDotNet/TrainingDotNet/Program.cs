0bb5711b0f3da465f65afdf9567844bf78bbd00a
Suman Tummanapally <suman.tummanapally@onewabash.com>
2024-02-28 16:38:23 +0530

    First Program

---
+++ workarea/DotNet Training/TrainingDotNet/TrainingDotNet/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingDotNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}

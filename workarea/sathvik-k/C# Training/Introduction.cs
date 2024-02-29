b9d507649d38072d72f537afe63e639bf53f2cb3
Sathvik903 <159435881+Sathvik903@users.noreply.github.com>
2024-02-29 10:44:52 +0530

    Create Introduction.cs

---
+++ workarea/sathvik-k/C# Training/Introduction.cs
@@ -0,0 +1,19 @@
+using System;
+
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+/*The line Console.WriteLine("Hello, World!"); is responsible for outputting the text "Hello, World!" to the console.
+
+Where,
+Console: It's a class in the System namespace that provides methods for interacting with the console, which is the text-based interface in which a program receives input and displays output.
+
+WriteLine: It's a method of the Console class used to display a line of text on the console. The WriteLine method automatically appends a newline character (\n) after the specified text, so the next output will be on a new line.
+
+"Hello, World!": This is the string that will be displayed on the console. In this case, it's a simple greeting message.
+*/

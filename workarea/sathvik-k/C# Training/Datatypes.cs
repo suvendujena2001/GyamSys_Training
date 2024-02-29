1426113538cfbd448f62e3486ec419b3e123d6b7
Sathvik903 <159435881+Sathvik903@users.noreply.github.com>
2024-02-29 10:56:56 +0530

    Update Datatypes.cs

--- workarea/sathvik-k/C# Training/Datatypes.cs
+++ workarea/sathvik-k/C# Training/Datatypes.cs
@@ -6,7 +6,19 @@ namespace HelloWorld
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");    
+      int myNum = 5;
+      double myDoubleNum = 5.99D;
+      char myLetter = 'D';
+      bool myBool = true;
+      string myText = "Hello";
+
+
+      Colsole.WriteLine(myNum);
+      Colsole.WriteLine(myDoubleNum);
+      Colsole.WriteLine(myLetter);
+      Colsole.WriteLine(myBool);
+      Colsole.WriteLine(myText);
+      
     }
   }
 }

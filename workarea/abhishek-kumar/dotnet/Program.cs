6caf02678000ad700d60986e0d03f3337ed4d466
Abhishek Kumar <abhishek.kumar2@gyansys.com>
2024-03-01 14:05:32 +0530

    'dotnet'

---
+++ workarea/abhishek-kumar/dotnet/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace dotnet;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Abhishek");
+
+
+        //if-else
+        Random dice = new Random();
+
+        int roll1 = dice.Next(1, 7);
+        int roll2 = dice.Next(1, 7);
+        int roll3 = dice.Next(1, 7);
+
+        int total = roll1 + roll2 + roll3;
+
+        Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+        if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+        {
+            Console.WriteLine("You rolled doubles! +2 bonus to total!");
+            total += 2;
+        }
+
+        if ((roll1 == roll2) && (roll2 == roll3))
+        {
+            Console.WriteLine("You rolled triples! +6 bonus to total!");
+            total += 6;
+        }
+
+        if (total >= 15)
+        {
+            Console.WriteLine("You win!");
+        }
+
+        if (total < 15)
+        {
+            Console.WriteLine("Sorry, you lose.");
+        }
+        //array
+
+        
+
+
+        string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+
+        Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+        Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+        Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+        fraudulentOrderIDs[0] = "F000";
+
+        Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+        Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+
+
+        int[] inventory = { 200, 450, 700, 175, 250 };
+        int sum = 0;
+        int bin = 0;
+        foreach (int items in inventory)
+        {
+            sum += items;
+            bin++;
+            Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+        }
+        Console.WriteLine($"We have {sum} items in inventory.");
+    }
+}

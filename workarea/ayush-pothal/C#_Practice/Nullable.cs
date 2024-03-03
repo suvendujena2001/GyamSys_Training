8ad904a578ba947b3a50944a24d006171f28264d
Ayush02240 <159024737+Ayush02240@users.noreply.github.com>
2024-03-03 20:49:12 +0530

    committing assignment

---
+++ workarea/ayush-pothal/C#_Practice/Nullable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaration & Initialization
+            int? a = null; 
+            int? b = 10;
+            bool? isBool = null;
+
+            //Properties of Nullable datatype
+            if (b.HasValue)
+            {
+                Console.WriteLine($"b is {b.Value}");
+            }
+            else
+            {
+                Console.WriteLine("b does not have any value");
+            }
+
+
+            // GetValueOrDefault(-1) Method
+            int? c = null;
+            Console.WriteLine($"c is {c.GetValueOrDefault(-1)}"); 
+
+
+            //Explicit Casting
+            int? d = 123;
+            int e = (int)d; 
+            Console.WriteLine($"e is {e}");
+
+
+
+        }
+
+    }
+}

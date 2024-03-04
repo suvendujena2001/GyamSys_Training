42bed7c136306ac0ad9e1c68b851d34384de59fd
Sathvik903 <159435881+Sathvik903@users.noreply.github.com>
2024-03-04 10:42:48 +0530

    If_Else_else-if.cs

--- workarea/sathvik-k/C# Training/If_Else_else-if.cs
+++ workarea/sathvik-k/C# Training/If_Else_else-if.cs
@@ -7,10 +7,10 @@ class Program
         Console.Write("Enter a number: ");
         string userInput = Console.ReadLine();
 
-        // Convert the user input to a double
+       
         if (double.TryParse(userInput, out double number))
         {
-            // Check if the number is positive, negative, or zero
+            
             if (number > 0)
             {
                 Console.WriteLine("The entered number is positive.");

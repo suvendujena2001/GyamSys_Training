d6019a93a26d2eda8c367bcfdea95a597a1aee20
Sathvik903 <159435881+Sathvik903@users.noreply.github.com>
2024-03-04 10:24:51 +0530

    Update If_Else_IfElse.cs

--- workarea/sathvik-k/C# Training/If_Else_IfElse.cs
+++ workarea/sathvik-k/C# Training/If_Else_IfElse.cs
@@ -1,44 +1,32 @@
-// C# Conditions and If Statements
-// C# supports the usual logical conditions from mathematics:
+using System;
 
-// Less than: a < b
-// Less than or equal to: a <= b
-// Greater than: a > b
-// Greater than or equal to: a >= b
-// Equal to a == b
-// Not Equal to: a != b
-// You can use these conditions to perform different actions for different decisions.
-
-// C# has the following conditional statements:
-
-// Use if to specify a block of code to be executed, if a specified condition is true
-// Use else to specify a block of code to be executed, if the same condition is false
-// Use else if to specify a new condition to test, if the first condition is false
-// Use switch to specify many alternative blocks of code to be executed
-// The if Statement
-// Use the if statement to specify a block of C# code to be executed if a condition is True.
-
-//Example:
-if (condition) 
+class Program
 {
-  // block of code to be executed if the condition is True
-}
-// Note that if is in lowercase letters. Uppercase letters (If or IF) will generate an error.
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        string userInput = Console.ReadLine();
 
-// In the example below, we test two values to find out if 20 is greater than 18. If the condition is True, print some text:
-
-// Example
-if (20 > 18) 
-{
-  Console.WriteLine("20 is greater than 18");
-}
-
-// We can also test variables:
-
-// Example
-int x = 20;
-int y = 18;
-if (x > y) 
-{
-  Console.WriteLine("x is greater than y");
+        // Convert the user input to a double
+        if (double.TryParse(userInput, out double number))
+        {
+            // Check if the number is positive, negative, or zero
+            if (number > 0)
+            {
+                Console.WriteLine("The entered number is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("The entered number is negative.");
+            }
+            else
+            {
+                Console.WriteLine("The entered number is zero.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+    }
 }

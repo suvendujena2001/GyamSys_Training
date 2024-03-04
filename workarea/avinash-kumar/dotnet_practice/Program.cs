2313a99edb53a9d33c6de60d3047b660bae02f8d
AvinashKumar <AvinashKumar@IN-7TH3NX3>
2024-03-04 11:58:16 +0530

    c#practice

--- workarea/avinash-kumar/dotnet_practice/Program.cs
+++ workarea/avinash-kumar/dotnet_practice/Program.cs
@@ -1,20 +1,93 @@
-﻿namespace dotnet_practice;
+﻿using System.Collections;
+using System.Numerics;
+
+namespace dotnet_practice;
 
 class Program
 {
+    public void result(params object[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+
+            Console.WriteLine(array[i]);
+        }
+    }
+     static void printArray(ArrayList arr)
+    {
+        foreach (var item in arr)
+        {
+            Console.WriteLine(item);
+        }
+
+    }
     static void Main(string[] args)
     {
-        //Console.WriteLine("Hello, Avinash");
+        /*Console.WriteLine("Hello, Avinash");
         DateTime a = DateTime.Now;
         double y = 10.7;
-        // int y=int.TryParse(x,out y);
+        int y=int.TryParse(x,out y);
         Console.WriteLine(y);
         bool z = Convert.ToBoolean(y);
         Console.WriteLine(z);
         string str = "Avinash";
         Console.WriteLine($"Hey {str} ");
 
+        bool? a = true;
+        Console.WriteLine(a ?? false);
+
+        Console.WriteLine((a.HasValue) ? "yes":"no");
+
+        //array
+
+        int[] arr = { 1, 2, 3, 4 };
+        foreach (var item in arr)
+        {   
+        if(item==3)
+            Console.WriteLine(item);
+     
+            
+        }
+        */
+
+
+        ArrayList arrayList = new ArrayList();
+        arrayList.Add('a');
+        arrayList.Add('b');
+        arrayList.Add(2);
+
+        printArray(arrayList);
+
+
+        int[,] array2DInitialization = { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+        Console.WriteLine(array2DInitialization.Length);
+        Console.WriteLine(array2DInitialization.GetLength(0));
+
+
+        int[][] jagged_arr = new int[4][];
+        jagged_arr[0] = new int[] { 1, 2, 3, 4 };
+        jagged_arr[1] = new int[] { 5, 6, 7 };
+        jagged_arr[2] = new int[] { 1, 2, 3, 4 };
+        jagged_arr[3] = new int[] { 5, 6, 7 };
+
+        for (int i = 0; i < jagged_arr.Length; i++)
+        {
+            for(int j = 0; j < jagged_arr[i].Length; j++)
+            {
+                Console.Write(jagged_arr[i][j] + " ");
+            }
+            Console.WriteLine();
+        }
+
        
+        Program p1 = new Program();
+
+        p1.result("Avinash", "Aditya",
+                        100, "100");
+
+
+
 
     }
+    
 }

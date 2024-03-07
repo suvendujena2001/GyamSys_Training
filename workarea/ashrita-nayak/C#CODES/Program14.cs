6364534ef00df5c3bae61d084119d2086d881382
Ashrita <ashrita.nayak@gyansys.com>
2024-03-07 15:17:25 +0530

    Queues & Stacks execution

---
+++ workarea/ashrita-nayak/C#CODES/Program14.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+class Rooftop
+{
+    static void Main()
+    {
+        Queue<int> myQueue = new Queue<int>();
+        myQueue.Enqueue(1);
+        myQueue.Enqueue(2);
+        myQueue.Enqueue(3);
+        while (myQueue.Count > 0)
+        {
+            int element = myQueue.Dequeue();
+            Console.WriteLine($"Dequeued: {element}");
+        }
+        Stack<int> myStack = new Stack<int>();
+        myStack.Push(1);
+        myStack.Push(2);
+        myStack.Push(3);
+        while (myStack.Count > 0)
+        {
+            int element = myStack.Pop();
+            Console.WriteLine($"Popped: {element}");
+        }
+    }
+}
+
+
+
+

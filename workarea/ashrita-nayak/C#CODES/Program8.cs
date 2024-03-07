99cc4a9a06a9a87f5251e20200377af2474db53f
Ashrita <ashrita.nayak@gyansys.com>
2024-03-07 11:13:23 +0530

    methods

---
+++ workarea/ashrita-nayak/C#CODES/Program8.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Method
+{
+    class Program
+    {
+        // method declaration
+         
+        public void display()
+        {
+            Console.WriteLine("The planet Earth is a beautiful place to live in.");
+        }
+        static void Main(string[] args)
+        { 
+            Program p1 = new Program();
+            p1.display();
+          ;
+
+            int z = MyMethod(23,89);
+            Console.WriteLine(z);
+        }
+        static int MyMethod(int x, int y)
+        {
+            return x + y;
+        }
+
+    }
+}
+
+

b869ff4a1acdcde1c51f1c0fe7797481f5678962
anand2165 <anand.mallikarjun@gyansys.com>
2024-03-11 10:12:25 +0530

    updated

--- workarea/anand-talekattu/C#-training/obj/Debug/net8.0/C#-training.AssemblyInfo.cs
+++ workarea/anand-talekattu/C#-training/obj/Debug/net8.0/C#-training.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#-training")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97887d256a798a2e40e15517b548d63fae018776")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee23bb83ada83ce0985d8b020cd7eda823484aa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#-training")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#-training")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

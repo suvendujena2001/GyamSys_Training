1d2ac8a4527e5c51c9fc95f1f8ca194fc62066d4
subhendujena2001 <159023329+subhendujena2001@users.noreply.github.com>
2024-03-12 09:50:08 +0530

    Single and Multi-level Inheritence

---
+++ workarea/subhendu-jena/C# .NET/Inheritance.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPs
+{
+    public class baseClass
+    {
+        public virtual void funResult()
+        {
+            Console.WriteLine("Base Class");
+        }
+    }
+    public class  derivedClass: baseClass
+    {
+        public void funWrite()
+        {
+            Console.WriteLine("Derived Class One");
+        }
+
+        public override void funResult()    //method overriding 
+        {
+            Console.WriteLine("Derived Class ONE");
+        }
+    }
+    public class derivedClass2 : derivedClass 
+    {
+        public void funRead()
+        {
+            Console.WriteLine("Derived Class 2");
+        }
+    }
+    internal class Inheritance
+    {
+        static void Main(string[] args)
+        {   
+            baseClass bc= new baseClass();
+            bc.funResult();
+
+            //single inheritance
+            derivedClass dc =new derivedClass();
+            dc.funResult();
+            dc.funWrite();
+
+            //multi-level inheritance
+            derivedClass2 dc2 = new derivedClass2();
+            dc2.funResult();
+            dc2.funWrite();
+            dc2.funRead();
+
+            derivedClass bdc= new derivedClass2(); //creating object of derivedClass using derivedClass2
+            bdc.funWrite();
+            bdc.funResult();
+
+            Console.Read();
+            //multiple inheritance
+                
+
+        }
+    }
+}

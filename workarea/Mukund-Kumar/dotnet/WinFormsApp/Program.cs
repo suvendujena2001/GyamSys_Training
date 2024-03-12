f90049db6656b84edd1c65c96d37e0ca0cdeecf5
mukund-24 <159022920+mukund-24@users.noreply.github.com>
2024-03-12 14:13:19 +0530

    Add files via upload

---
+++ workarea/Mukund-Kumar/dotnet/WinFormsApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // initialize variables - graded assignments 
+            int currentAssignments = 5;
+
+            int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
+            int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
+            int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
+            int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
+
+            // Student names
+            string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+
+            foreach (string name in studentNames)
+            {
+                if (name == "Sophia")
+                {
+                    int sophiaSum = 0;
+                    decimal sophiaScore;
+
+                    foreach (int score in sophiaScores)
+                    {
+                        // add the exam score to the sum
+                        sophiaSum += score;
+                    }
+
+                    sophiaScore = (decimal)(sophiaSum) / currentAssignments;
+
+                    Console.WriteLine("Student\t\tGrade\n");
+                    Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+                }
+            }
+
+            Console.WriteLine("Press the Enter key to continue");
+            Console.ReadLine();
+        }
+    }
+}

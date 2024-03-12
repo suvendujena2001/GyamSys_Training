78e05c68e6aba02577196efe3f2c6dd276ae2b1a
Manohar2172 <159028962+Manohar2172@users.noreply.github.com>
2024-03-12 14:17:29 +0530

    Week-2 .NET

---
+++ workarea/Manohar-V/.NET-Training/Week-2/Week-2/Classes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2
+{
+    internal class Classes
+    {
+        /*        //Declaring an object of type MyClass.
+                MyClass mc = new MyClass();
+
+                //Declaring another object of the same type, assigning it the value of the first object.
+                MyClass mc2 = mc;*/
+
+        // Creating objects
+        //  object1 = new Customer();
+        // Although they're sometimes used interchangeably, a class and an object are different things. A class defines a type of object, but it isn't an object itself.An object is a concrete entity based on a class, and is sometimes referred to as an instance of a class.
+
+        /*public class Container
+        {
+            private int _capacity;
+
+            public Container(int capacity) => _capacity = capacity;
+        }*/
+
+        public class Container(int capacity)
+        {
+            private int _capacity = capacity;
+        }
+    }
+}
---
+++ workarea/Manohar-V/.NET-Training/Week-2/Week-2/Exception_Handling.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2
+{
+     public class Exception_Handling
+    {
+        
+            public static void Main()
+            {
+                try
+                {
+                    string? s = null;
+                    Console.WriteLine(s.Length);
+                }
+                catch (Exception e) when (LogException(e))
+                {
+                }
+                Console.WriteLine("Exception must have been handled");
+            }
+
+            private static bool LogException(Exception e)
+            {
+                Console.WriteLine($"\tIn the log routine. Caught {e.GetType()}");
+                Console.WriteLine($"\tMessage: {e.Message}");
+                return false;
+            }
+
+            int GetInt(int[] array, int index)
+            {
+                try
+                {
+                    return array[index];
+                }
+                catch (IndexOutOfRangeException e) when (index < 0)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        "Parameter index cannot be negative.", e);
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        "Parameter index cannot be greater than the array size.", e);
+                }
+            }
+        
+    }
+}

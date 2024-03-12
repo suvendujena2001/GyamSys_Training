6ddf44e12e2f17fc180f5bd8d68d1935d2d8f8b7
Ashrita <ashrita.nayak@gyansys.com>
2024-03-12 14:20:21 +0530

    Multiple inheritance example

---
+++ workarea/ashrita-nayak/C#CODES/Program18.cs
@@ -0,0 +1,33 @@
+﻿using System;
+interface Bicycle
+{
+    void FirstMethod();
+}
+interface Scooty
+{
+    void SecondMethod();
+}
+class Road : Bicycle, Scooty
+{
+    public void FirstMethod()
+    {
+        Console.WriteLine("Do you have a bicycle to lend me?");
+    }
+
+    public void SecondMethod()
+    {
+        Console.WriteLine("Please take me on a " +
+            "scooty ride around the village.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Road myObj = new Road();
+        myObj.FirstMethod(); 
+        myObj.SecondMethod(); 
+    }
+}
+

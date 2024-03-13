380bc9b229be38f332ac16269ef2a50a7d2ce35c
SoumyadipDhara <SoumyadipDhara@gyansys.com>
2024-03-13 15:10:17 +0530

    Windows EntityFramework

---
+++ workarea/soumyadip-dhara/C#_Training/WindowsFormsApp/WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp4.masterDataSetTableAdapters;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'masterDataSet.EMPLOYEE' table. You can move, or remove it, as needed.
+            this.eMPLOYEETableAdapter.Fill(this.masterDataSet.EMPLOYEE);
+
+        }
+    }
+}

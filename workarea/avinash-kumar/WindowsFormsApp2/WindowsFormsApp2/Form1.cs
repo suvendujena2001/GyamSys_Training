aae12c5a4996fe9db90cdb59e81da81429c022a0
AvinashKumar <AvinashKumar@IN-7TH3NX3>
2024-03-14 16:47:58 +0530

    added progress bar which shows the status of click on the grid box or combo box  and text field in winforms

--- workarea/avinash-kumar/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ workarea/avinash-kumar/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,6 +15,7 @@ namespace WindowsFormsApp2
     public partial class Form1 : Form
     {
         EntityFrameWork obj = new EntityFrameWork();
+        
 
         public Form1()
         {
@@ -29,6 +31,7 @@ namespace WindowsFormsApp2
             comboBox1.DataSource = list;
             comboBox1.DisplayMember = "Name";
             comboBox1.ValueMember = "NationalIDNumber";
+            progressBar1.Value= 0;
 
 
         }
@@ -36,7 +39,35 @@ namespace WindowsFormsApp2
         
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+           var a=comboBox1.GetItemText(comboBox1.SelectedIndex);
+            List<Employee> list = obj.Employees.ToList();
+            textBox1.Text =a;
             
+            progressBar1.Value += 10;
+            if(progressBar1.Value == 100)
+            {
+                this.Close();
+            }
+            if(progressBar1.Value == 90)
+            {
+                MessageBox.Show("You are left with 1 attempt ");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 frm=new Form2();
+            frm.Show();
+        }
+
+        private void exiteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }

3e5188393600e9c0dcabd054fb7821394bef771e
Ashrita <ashrita.nayak@gyansys.com>
2024-03-18 10:49:41 +0530

    TreeView Execution with nodes

---
+++ workarea/ashrita-nayak/C#CODES/WINFORMS4/Reflectobjects.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeviewProgram1
+{
+    public class Reflectobjects
+    {
+		public void LoadForm(string form, Form F)
+		{
+            try
+            {
+                Type objectType = (from asm in AppDomain.
+           CurrentDomain.GetAssemblies()
+           from type in asm.GetTypes()
+           where type.IsClass && type.Name == form select
+           type).Single();
+                object obj = Activator.CreateInstance(objectType);
+                Form f = obj as Form;
+                f.ShowDialog();
+            }
+            catch (global::System.Exception)
+            {
+
+               
+            }
+        }
+      
+
+    }
+}

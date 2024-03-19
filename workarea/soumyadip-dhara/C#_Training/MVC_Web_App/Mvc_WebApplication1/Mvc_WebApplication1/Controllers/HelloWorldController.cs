4e8f5efec1d69bd9735ab1c9d4330943a1d8d9fc
SoumyadipDhara <SoumyadipDhara@gyansys.com>
2024-03-19 15:06:59 +0530

    First MVC Web App

---
+++ workarea/soumyadip-dhara/C#_Training/MVC_Web_App/Mvc_WebApplication1/Mvc_WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class EmployeeController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
+  

464c09e84e5c95728d1238bfdec2fbc4a4837284
Abhishek Kumar <abhishek.kumar2@gyansys.com>
2024-03-20 15:20:27 +0530

    mvc practice

---
+++ workarea/abhishek-kumar/dotnet/MvcPractice/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcPractice.Models
+{
+    public class Employee
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public int? Salary { get; set; }
+        public string? Gender { get; set; }
+        public int? Department { get; set; }
+    }
+}

d59affbcd2ca9ca531c0fc2d69b9347bf951dfb7
SuvamP2001 <suvam.pattanaik@gyansys.com>
2024-03-20 12:25:59 +0530

    web-application ASP.NET

---
+++ workarea/suvam-pattanaik/ASP.NET/WebApplication1/WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeDbContext dbContext = new EmployeeDbContext();
+
+        public IActionResult Index()
+        {
+            List<Employee> employees = dbContext.Employees;
+            return View(employees);
+        }
+
+    }
+}
---
+++ workarea/suvam-pattanaik/ASP.NET/WebApplication1/WebApplication1/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+
+        public int? Salary { get; set; }
+
+        public string? Gender { get; set; }
+        public string? Department { get; set; }
+
+    }
+}

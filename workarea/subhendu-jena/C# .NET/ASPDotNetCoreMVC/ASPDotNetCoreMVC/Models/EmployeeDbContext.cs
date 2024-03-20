e32aa25d5d695452bc88254815363226fd716fd6
Subhendu Kumar Jena <subhendu.jena@gyansys.com>
2024-03-20 14:10:36 +0530

    Asp.NET mvc update

---
+++ workarea/subhendu-jena/C# .NET/ASPDotNetCoreMVC/ASPDotNetCoreMVC/Models/EmployeeDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace ASPDotNetCoreMVC.Models
+{
+    public class EmployeeDbContext
+    {
+        public EmployeeDbContext()
+        {
+
+        }
+
+        public List<Employee> Employees = new List<Employee>
+        {
+             new Employee { Id =1, Name = "Subhendu", Salary = 50000, Department = "HR", Gender = "Male" },
+             new Employee { Id =2, Name = "Swagat", Salary = 60000, Department = "Payroll", Gender = "Male"  },
+             new Employee { Id = 3, Name = "Swayam", Salary = 40000, Department = "IT", Gender = "Male" },
+             new Employee { Id = 4, Name = "Ayush", Salary = 70000, Department = "Support", Gender = "Male" }
+        };
+    }
+}

1972e0b005d167a6a5b3bc98e4188996efac2fe0
Aditya Raj <aditya.thakur@gyansys.com>
2024-03-20 15:03:21 +0530

    Login

---
+++ workarea/aditya-raj-thakur/csharp/WindowsApp/WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeDbContext db = new EmployeeDbContext();
+
+    public IActionResult Index() //get
+        {
+            List<Employee> employee = db.Employees;
+            return View(employee);
+        }
+    public ActionResult Details(int id)
+        {
+            List<Employee> employee = db.Employees;
+            return View("Index", employee);
+        }
+
+    }
+
+
+}

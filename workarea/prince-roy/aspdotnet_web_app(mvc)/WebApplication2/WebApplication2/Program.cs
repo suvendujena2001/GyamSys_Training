808620ea99396b8d9277cb272018f7c0c875a1e5
Prince Roy <princeku.ray143@gmail.com>
2024-03-25 14:11:52 +0530

    create http get search method on user input

--- workarea/prince-roy/aspdotnet_web_app(mvc)/WebApplication2/WebApplication2/Program.cs
+++ workarea/prince-roy/aspdotnet_web_app(mvc)/WebApplication2/WebApplication2/Program.cs
@@ -20,8 +20,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+/*
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+*/
+app.MapControllers();
 
 app.Run();

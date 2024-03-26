073c7279967d80e98b1083804618a6eeb39241af
Ashrita <ashrita.nayak@gyansys.com>
2024-03-26 19:52:51 +0530

    STUDENT PORTAL

---
+++ workarea/ashrita-nayak/CRUD_ASP.NET/Data/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUD_ASP.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_ASP.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}

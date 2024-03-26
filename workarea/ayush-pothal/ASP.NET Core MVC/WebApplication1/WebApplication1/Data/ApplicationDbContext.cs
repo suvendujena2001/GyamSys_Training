fe8cdbc76b900e560cd905a8ef9e71a4b2a1a5e0
Ayush Kumar Pothal <ayush.pothal@gyansys.com>
2024-03-26 15:11:42 +0530

    MVC Practice

---
+++ workarea/ayush-pothal/ASP.NET Core MVC/WebApplication1/WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : DbContext
+    { 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+       
+                         
+        }
+
+        public DbSet<Category> Categories{ get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Ayush", DisplayOrder = 1 },
+            new Category { Id = 2, Name = "Swayam", DisplayOrder = 2 },
+            new Category { Id = 3, Name = "Subhasish", DisplayOrder = 3 }
+            );
+               
+               
+        }
+    }
+}

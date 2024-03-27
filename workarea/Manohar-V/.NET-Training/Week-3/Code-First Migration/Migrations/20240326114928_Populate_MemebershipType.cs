f1c6907ed13e34fe969e8d3ccee76f70dcea410d
Manohar2172 <159028962+Manohar2172@users.noreply.github.com>
2024-03-27 10:56:32 +0530

    Added Code-First

---
+++ workarea/Manohar-V/.NET-Training/Week-3/Code-First Migration/Migrations/20240326114928_Populate_MemebershipType.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC_1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Populate_MemebershipType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO MemebershipType (Id,SignUpFee,DurationInMonths,DiscountRate) VALUES (1,1000,3,10) ");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ workarea/Manohar-V/.NET-Training/Week-3/Code-First Migration/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_1.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+       
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MemebershipType MemebershipType { get; set; }    
+        public byte MemebershipTypeId { get; set; }
+    }
+}

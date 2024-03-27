ad02f5138f696e54df1f853b03236699adf07d1c
KalpanaPanigrahi <Kalpana.panigrahi@gyansys.com>
2024-03-27 10:12:55 +0530

    MVC Practice

---
+++ workarea/Kalpana-panigrahi/ASP.NET C#/Strongly_typed_view_MVC/Strongly_typed_view_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Strongly_typed_view_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

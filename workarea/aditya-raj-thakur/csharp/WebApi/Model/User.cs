b4aba435fd502b105847b22230912b5e39a49928
Aditya Raj <aditya.thakur@gyansys.com>
2024-04-01 13:34:01 +0530

    WebApiDemo

---
+++ workarea/aditya-raj-thakur/csharp/WebApi/Model/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Model
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int  UserId { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public String Gender { get; set; }
+
+
+    }
+}

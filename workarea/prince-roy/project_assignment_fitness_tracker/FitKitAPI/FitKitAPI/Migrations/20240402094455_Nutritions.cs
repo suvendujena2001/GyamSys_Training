713a2d34338789d08e94b796d403691ccadad2b0
Prince Roy <princeku.ray143@gmail.com>
2024-04-02 15:30:41 +0530

    create API and FitKit Database

---
+++ workarea/prince-roy/project_assignment_fitness_tracker/FitKitAPI/FitKitAPI/Migrations/20240402094455_Nutritions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitKitAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Nutritions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Nutrition",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Calories = table.Column<double>(type: "float", nullable: false),
+                    Carbohydrates = table.Column<double>(type: "float", nullable: false),
+                    Protein = table.Column<double>(type: "float", nullable: false),
+                    Micronutrients = table.Column<double>(type: "float", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<int>(type: "int", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nutrition", x => x.UserId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Nutrition");
+        }
+    }
+}
---
+++ workarea/prince-roy/project_assignment_fitness_tracker/FitKitAPI/FitKitAPI/Models/Workout.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitKitAPI.Models
+{
+    public class Workout
+    {
+        [Key]
+        public int UserId { get; set; }
+        public int ExerciseId { get; set; }
+        public string ExerciseType { get; set; }
+        public double Duration { get; set; }
+        public double Intensity { get; set; }
+        public double Sets { get; set; }
+        public double Repeatitions { get; set; }
+        public string Notes { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+    }
+}

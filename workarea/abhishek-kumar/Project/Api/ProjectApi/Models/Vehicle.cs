38f65a1157fb4facc0a7cfdffa51ee1d85a856c5
Abhishek Kumar <abhishek.kumar2@gyansys.com>
2024-04-02 12:44:14 +0530

    api created

---
+++ workarea/abhishek-kumar/Project/Api/ProjectApi/Models/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectApi.Models;
+
+public partial class Vehicle
+{
+    public int Id { get; set; }
+
+    public string? Model { get; set; }
+
+    public string? Colour { get; set; }
+
+    public string? NumberPlate { get; set; }
+
+    public string? FuelType { get; set; }
+
+    public string? TransmissionType { get; set; }
+
+    public string? Cylinders { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+
+    public int? CreatedBy { get; set; }
+
+    public DateTime? ModifiedDate { get; set; }
+
+    public int? ModifiedBy { get; set; }
+
+    public virtual ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
+
+    public virtual ICollection<Report> Reports { get; set; } = new List<Report>();
+}

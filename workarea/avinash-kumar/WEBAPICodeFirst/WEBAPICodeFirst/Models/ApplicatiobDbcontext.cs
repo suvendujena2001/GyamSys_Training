4c4e0a27d7ae9082ba91fd3df020e8fdcb707fea
AvinashKumar <AvinashKumar@IN-7TH3NX3>
2024-04-02 12:41:35 +0530

    demo web api

---
+++ workarea/avinash-kumar/WEBAPICodeFirst/WEBAPICodeFirst/Models/ApplicatiobDbcontext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;    
+using WEBAPICodeFirst.Models;
+
+namespace WEBAPICodeFirst.Models
+{
+    public class ApplicatiobDbcontext: DbContext
+    {
+        public ApplicatiobDbcontext(DbContextOptions<ApplicatiobDbcontext>options):base(options) 
+        
+        {
+
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Exam> Exams { get; set; }
+             public DbSet<Question> Questions { get; set; }
+    }
+}
---
+++ workarea/avinash-kumar/WEBAPICodeFirst/WEBAPICodeFirst/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿namespace WEBAPICodeFirst.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        public string Name { get; set; } 
+        public string Email { get; set; }
+
+        public string Password { get; set; } 
+
+        public DateTime? CreatedDate { get; set; }
+
+        public int? CreatedBy { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+        public int? ModifiedBy { get; set; }
+
+        public bool? Active { get; set; }
+      
+    }
+}

30fa023fb7dec7389ba7c3e9aeea3b7a92f4ebac
Subhendu Kumar Jena <subhendu.jena@gyansys.com>
2024-04-05 15:56:38 +0530

    API-Entity framework completion

---
+++ workarea/subhendu-jena/CarRentalSystem/CarRentalSystem/CarRentalSystem/Controllers/CustomerSupportsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CarRentalSystem.Models;
+
+namespace CarRentalSystem.Controllers
+{
+    public class CustomerSupportsController : Controller
+    {
+        private readonly CarRentalContext _context;
+
+        public CustomerSupportsController(CarRentalContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CustomerSupports
+        public async Task<IActionResult> Index()
+        {
+            var carRentalContext = _context.CustomerSupports.Include(c => c.User);
+            return View(await carRentalContext.ToListAsync());
+        }
+
+        // GET: CustomerSupports/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customerSupport = await _context.CustomerSupports
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.SupportId == id);
+            if (customerSupport == null)
+            {
+                return NotFound();
+            }
+
+            return View(customerSupport);
+        }
+
+        // GET: CustomerSupports/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId");
+            return View();
+        }
+
+        // POST: CustomerSupports/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SupportId,UserId,Timestamp,Channel,Message,CreatedDate,CreatedBy,UpdatedDate,UpdatedBy")] CustomerSupport customerSupport)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(customerSupport);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", customerSupport.UserId);
+            return View(customerSupport);
+        }
+
+        // GET: CustomerSupports/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customerSupport = await _context.CustomerSupports.FindAsync(id);
+            if (customerSupport == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", customerSupport.UserId);
+            return View(customerSupport);
+        }
+
+        // POST: CustomerSupports/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SupportId,UserId,Timestamp,Channel,Message,CreatedDate,CreatedBy,UpdatedDate,UpdatedBy")] CustomerSupport customerSupport)
+        {
+            if (id != customerSupport.SupportId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customerSupport);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerSupportExists(customerSupport.SupportId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", customerSupport.UserId);
+            return View(customerSupport);
+        }
+
+        // GET: CustomerSupports/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customerSupport = await _context.CustomerSupports
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.SupportId == id);
+            if (customerSupport == null)
+            {
+                return NotFound();
+            }
+
+            return View(customerSupport);
+        }
+
+        // POST: CustomerSupports/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var customerSupport = await _context.CustomerSupports.FindAsync(id);
+            if (customerSupport != null)
+            {
+                _context.CustomerSupports.Remove(customerSupport);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomerSupportExists(int id)
+        {
+            return _context.CustomerSupports.Any(e => e.SupportId == id);
+        }
+    }
+}
---
+++ workarea/subhendu-jena/CarRentalSystem/CarRentalSystem/CarRentalSystem/Models/Maintenance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarRentalSystem.Models;
+
+public partial class Maintenance
+{
+    public int MaintenanceId { get; set; }
+
+    public int? VehicleId { get; set; }
+
+    public DateTime? MaintenanceDate { get; set; }
+
+    public string? Description { get; set; }
+
+    public decimal? Cost { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+
+    public int? CreatedBy { get; set; }
+
+    public DateTime? UpdatedDate { get; set; }
+
+    public string? UpdatedBy { get; set; }
+
+    public virtual Vehicle? Vehicle { get; set; }
+}

0b6931c8e64ea8f297f3226430a6f8b1fdda5897
Prince Roy <princeku.ray143@gmail.com>
2024-04-07 19:19:04 +0530

    update API

--- workarea/prince-roy/project_assignment_fitness_tracker/FitKitAPI/FitKitAPI/Models/UserCredential.cs
+++ workarea/prince-roy/project_assignment_fitness_tracker/FitKitAPI/FitKitAPI/Models/UserCredential.cs
@@ -6,6 +6,8 @@ namespace FitKitAPI.Models
     {
         [Key]
         public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string UserName { get; set; }
         public string UserEmail { get; set; }
         public string Password { get; set; }

d307bf1514803e2b9278cfb64f278d5592510824
AvinashKumar <AvinashKumar@IN-7TH3NX3>
2024-04-11 11:46:51 +0530

    Created react Assignment Blog web api for the backend

---
+++ workarea/avinash-kumar/ReactBlogProject/BlogWebApi/BlogWebApi/Models/BlogDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogWebApi.Models
+{
+    public class BlogDbContext:DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options) { }
+        public DbSet<Blog> Blogs { get; set; } 
+    }
+}

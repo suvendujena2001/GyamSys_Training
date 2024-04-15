92a14a5b1a0b3ab383d5c4e37bc0529debc2479d
Aditya Raj <aditya.thakur@gyansys.com>
2024-04-15 19:01:46 +0530

    BlogProject

---
+++ workarea/aditya-raj-thakur/React/BlogAssignment/BlogApi/Model/BlogDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApi.Model
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}

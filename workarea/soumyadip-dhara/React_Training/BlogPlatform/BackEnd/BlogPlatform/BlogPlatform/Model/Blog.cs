58f56f5083a7d81e2adcc069706f1931dbf140ef
SoumyadipDhara <SoumyadipDhara@gyansys.com>
2024-04-16 15:23:43 +0530

    Added Api for BlogPlatform Project using React

---
+++ workarea/soumyadip-dhara/React_Training/BlogPlatform/BackEnd/BlogPlatform/BlogPlatform/Model/Blog.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogPlatform.Model
+{
+    public class Blog
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title {  get; set; }
+        public string Description {  get; set; }
+        public string Image {  get; set; }
+    }
+}
---
+++ workarea/soumyadip-dhara/React_Training/BlogPlatform/BackEnd/BlogPlatform/BlogPlatform/Model/ClassDbContext.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogPlatform.Model
+{
+    public class ClassDbContext:DbContext
+    { 
+       public  ClassDbContext(DbContextOptions<ClassDbContext> options) : base(options) {
+        }
+        public DbSet<Blog> Blogs { get; set; }  
+    }
+}

f79e0a9062ee1f30ef2ccc14a317ee32804317cd
SwagatSahu2 <159022955+SwagatSahu2@users.noreply.github.com>
2024-04-24 16:28:11 +0530

    Update

--- workarea/swagat-sahu/Angular Training/Form Assignment/src/app/form/form.component.ts
+++ workarea/swagat-sahu/Angular Training/Form Assignment/src/app/form/form.component.ts
@@ -13,15 +13,21 @@ export class FormComponent {
 
   email = new FormControl('', [Validators.required, Validators.email]);
   name = new FormControl('', [Validators.required, Validators.maxLength(30)]);
-  phone = new FormControl('', [Validators.required, Validators.pattern("[0-9]{10}")]);
+  phone = new FormControl('', [Validators.required,Validators.pattern("[0-9]{10}")] );
+  gender= new FormControl();
+  skills= new FormControl();
 
-  emailDetail(){
-    console.log(this.email);
-  }
-  nameDetail(){
-    console.log(this.name);
-  }
-  phoneDetail(){
-    console.log(this.phone);
+
+  onSubmit() {
+    if (this.name.valid && this.email.valid && this.phone.valid) {
+      console.log('Name:', this.name.value);
+      console.log('Email:', this.email.value);
+      console.log('Phone:', this.phone.value);
+      console.log('gender:', this.gender.valid);
+      console.log('skills:', this.skills.valid);
+      
+    } else {
+      console.log('Form is invalid. Please fill out all required fields correctly.');
+    }
   }
 }

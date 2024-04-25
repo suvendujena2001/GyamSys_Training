60c842d0bcd5debb41ea25f4f10c32d24412b497
Abhishek Kumar <abhishek.kumar2@gyansys.com>
2024-04-25 18:56:30 +0530

    Angular form update

--- workarea/abhishek-kumar/Angular/Angular-Form/src/app/app.component.ts
+++ workarea/abhishek-kumar/Angular/Angular-Form/src/app/app.component.ts
@@ -1,27 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { FormComponent } from './form/form.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
+  imports: [RouterOutlet,FormComponent , CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular-Form';
-  constructor(){}
-
-  genderOptions=['Male', 'Female'];
-  skillsOptions=['Angular','Html' , 'CSS' , 'JavaScript' ,'C#' ,'React' ];
-
-  form = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.maxLength(20)]),
-    email: new FormControl('', [Validators.required ,Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}')]),
-    phone: new FormControl('', [Validators.required, Validators.pattern(/^\d{10}$/)]),
-    gender: new FormGroup(''),
-    skills: new FormGroup(''), 
-  })
+  
 }
--- workarea/abhishek-kumar/Angular/Angular-Form/src/app/app.routes.ts
+++ workarea/abhishek-kumar/Angular/Angular-Form/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { UserComponent } from './user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: FormComponent,
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+  },
+];
